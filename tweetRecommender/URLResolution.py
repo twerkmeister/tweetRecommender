22282d38267850c06186b117662dc7d96871da00
easten20 <easten_20@hotmail.com>
2014-04-14 20:56:44 +0200

    update URLResolution file

--- tweetRecommender/URLResolution.py
+++ tweetRecommender/URLResolution.py
@@ -6,28 +6,46 @@ Created on Apr 14, 2014
 
 import urllib.request
 from mongoconnector import mongo
+from bson.objectid import ObjectId
                                 
     
 def redirectExists(url):            
-    cursor = mongo.db.redirects.find_one({"from" : url})                
-    return bool(cursor)
+    data = mongo.db.redirects.find_one({"from" : url})                
+    return bool(data)
 
-def insertIndexes(url):
-    "'not yet done'"
-    if not (mongo.db.webpages_tweets.find_one({"url": url})):    
-        mongo.db.webpages_tweets.insert("we")
+def getRedirects(url):
+    data = mongo.db.redirects.find_one({"from" : url})
+    return data
+
+def webpagesTweetsExist(url):
+    cursor = mongo.db.webpages_tweets.find_one({"url" : url})                
+    return bool(cursor)    
+        
+"""URL Resolution method get from tweets in MongoDB"""        
+def URLResolutionMongoDB(objectId):
+    data = mongo.db.tweets.find_one({"_id" : objectId})
+    url = data["urls"][0]
+    urlData = getRedirects(url)    
     
-def URLResolution(url):
-    if not (redirectExists(url)):        
+    """ check if url exist in redirects collection """
+    if not (bool(urlData)):        
+        """ GET redirect url """
         response = urllib.request.urlopen(url)
         urlLink = response.geturl()
-        mongo.db.redirects.insert({"from" : url, "url" : urlLink})                  
-    else:            
-        print("exist")    
+        mongo.db.redirects.insert({"from" : url, "url" : urlLink})                        
+    else:
+        urlLink = urlData["to"]    
+       
+    """" check whether tweets exist inside the webpages_tweets table"""
+    webpage = mongo.db.webpages_tweets.find_one({"url" : urlLink})
+    if (bool(webpage)):
+        """ use addToSet so there is no duplication in the array """
+        mongo.db.webpages_tweets.update({"url" : urlLink}, {"$addToSet" : {"tweets" : objectId}})
+    else:
+        mongo.db.webpages_tweets.insert({"url" : urlLink,  "tweets" : [objectId]})
+    
+    print("tweets resolution done")    
         
-if __name__ == '__main__':    
-    URLResolution('http://spon.de/aecUM')    
-    
-    
-    
+if __name__ == '__main__':            
+    URLResolutionMongoDB(ObjectId("52adb77a00323226c3b871dc"))          
     
