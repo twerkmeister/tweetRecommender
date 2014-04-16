1a5e212927ba43b33d3c0a983177eae18330e254
christian.godde <christian.godde@student.hpi.uni-potsdam.de>
2014-04-16 11:11:50 +0200

    filtering

--- tweetRecommender/tweetfilter.py
+++ tweetRecommender/tweetfilter.py
@@ -1,4 +1,5 @@
 from config import config
+import re
 
 def check_size(text):
 	cleaned = clean_tweet(text)
@@ -8,6 +9,14 @@ def clean_tweet(text):
 	#XXX remove hashtags and artifacts
 	return text
 
-def check_author_credibility(tweet):
-	#XXX check #followers etc.
+def check_author_credibility(user):
+	if not user['verified']:
+		return False
+	if not whitelist(user['user_id']):
+		return False
+	if blacklist(user['user_id']):
+		return False
+	if user['followers_count'] < config['followers_count']:
+		return False
+
 	return True
