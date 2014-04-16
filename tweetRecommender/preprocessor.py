0df46c06d59f103f7bc50e8e8b04529abdd7e3cd
christian.godde <christian.godde@student.hpi.uni-potsdam.de>
2014-04-16 15:21:52 +0200

    multiprocessing for preprocessor

--- tweetRecommender/preprocessor.py
+++ tweetRecommender/preprocessor.py
@@ -2,31 +2,39 @@ from tweetRecommender.mongo import mongo
 import tweetRecommender.web as webprocessor
 import tweetRecommender.resolution as tweetprocessor
 
+import multiprocessing as mp
+
 def process_initial_subset(process_tweets=True, process_web=False):
     if process_tweets:
+    	q = mp.Queue()
+    	p = mp.Process(target=process_tweet, args=(q,))
+    	p.start()
         tweets = mongo.db.tweets.find()
         for tweet in tweets:
-            process_tweet(tweet)
+            q.put(tweet.get('_id'))
+        p.join()
 
     if process_web:
         webpages = mongo.db.webpages.find()
         for webpage in webpages:
             process_webpage(webpage)
 
-def process_tweet(tweet):
-    tweetprocessor.handle_mongo(tweet.get('_id'))
+def process_tweet(tweet_queue):
+	tweet_id = tweet_queue.get()
+    tweetprocessor.handle_mongo(tweet_id)
 
 def process_webpage(webpage):
     webprocessor.handle(webpage.get('url'))
 
 if __name__ == '__main__':
 	import sys
-	if len(sys.argv) != 1 or len(sys.argv) != 3:
-    	print("preprocessor [<process_tweets> <process_web>]")
-    	sys.exit(1)
-    	
-    if len(sys.argv) == 1
+
+	if ((len(sys.argv) != 1) and (len(sys.argv) != 3)):
+		print "preprocessor [<process_tweets> <process_web>]"
+		sys.exit(1)
+
+	if len(sys.argv) == 1:
 		process_initial_subset()
-	if len(sys.argv) == 3
+	if len(sys.argv) == 3:
 		process_initial_subset(sys.argv[1], sys.argv[2])
 
