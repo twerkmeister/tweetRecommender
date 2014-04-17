111d758dfe4d4dc7c9d5f37b8409d0dd7be5315c
Robert Lehmann <mail@robertlehmann.de>
2014-04-17 08:05:25 +0200

    Fix JS syntax.

--- mongoscripts/setup_indices.js
+++ mongoscripts/setup_indices.js
@@ -1,3 +1,3 @@
-db.webpages_tweets.ensure_index({url: "hashed"})
-db.webpages.ensure_index({url: "hashed"})
-db.redirects.ensure_index({from: "hashed"})
+db.webpages_tweets.ensureIndex({url: "hashed"})
+db.webpages.ensureIndex({url: "hashed"})
+db.redirects.ensureIndex({from: "hashed"})
