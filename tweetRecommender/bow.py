a36b5c2590d54a1f1776e5d024960e52859cce77
christian.godde <christian.godde@student.hpi.uni-potsdam.de>
2014-04-22 16:52:34 +0200

    dictionary and corpus

--- tweetRecommender/bow.py
+++ tweetRecommender/bow.py
@@ -18,7 +18,7 @@ stopwords = stopwords.words('english')
 stopwords.extend(["'re", "n't", "'s"])
 
 def subset():
-	return mongo.db.webpages.find().skip(172).limit(100)
+	return mongo.db.webpages.find().skip(172).limit(1000)
 
 def tokenize(doc):
 	return [ps.stem(w) for s in sent_tokenize(doc["content"].lower()) for w in word_tokenize(s)]
@@ -37,3 +37,6 @@ if __name__ == '__main__':
 	corpus = MongoCorpus(dictionary)
 	
 	corpora.MmCorpus.serialize("tmp/corpus.mm", corpus)
+
+	model = models.ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=10)
+	model.print_topics(2)
