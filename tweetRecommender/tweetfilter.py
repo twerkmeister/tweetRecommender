17549495a06b072e0ff62bc1f23f4975bf1002f7
easten20 <easten_20@hotmail.com>
2014-04-30 10:41:43 +0200

    update feature clean tweets message

--- tweetRecommender/tweetfilter.py
+++ tweetRecommender/tweetfilter.py
@@ -1,4 +1,5 @@
 from tweetRecommender.config import config
+import re
 
 def check(tweet):
     return check_size(tweet.text) and check_author_credibility(tweet)
@@ -7,9 +8,16 @@ def check_size(text):
 	cleaned = clean_tweet(text)
 	return len(cleaned) >= config['tweet_min_length']
 
-def clean_tweet(text):
-	#XXX remove hashtags and artifacts
-	return text
+def clean_tweet(text):	     
+    # thanks to http://ravikiranj.net/drupal/201205/code/machine-learning/how-build-twitter-sentiment-analyzer
+    text = text.lower() #Convert to lower case    
+    text = re.sub('((www\.[\s]+)|(https?://[^\s]+))','',text) #Convert www.* or https?://* to EMPTY STRING    
+    text = re.sub('@[^\s]+','',text) #Convert @username to EMPTY STRING    
+    text = re.sub('[\s]+', ' ', text) #Remove additional white spaces    
+    text = re.sub(r'#([^\s]+)', r'\1', text) #Replace #word with word
+    text = re.sub(r'\brt\b','', text) #Replace retweet with empty string    
+    text = text.strip('\'"') #trim        
+    return text
 
 def check_author_credibility(user):
 	if not user['verified']:
@@ -24,3 +32,6 @@ def check_author_credibility(user):
 	#	return False
 
 	return True
+    
+if __name__ == "__main__":        
+    print clean_tweet("RT @RevolutionSyria: (08-15-13) #Daraa #Syria l Rebels Lay Siege to #Assad Hagana Battalion by border in Daraa http://t.co/RMfQhlU2Jr http://www.test.com")    
