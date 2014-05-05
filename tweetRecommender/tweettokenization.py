b5fa9849dbf9efbcbde80f22d3936ed76668b9a1
easten20 <easten_20@hotmail.com>
2014-05-05 14:10:14 +0200

    add tweettokenization
    implementation of bag of word on sample tweets data

---
+++ tweetRecommender/tweettokenization.py
@@ -0,0 +1,43 @@
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from tweetRecommender.mongo import mongo
+from bson.objectid import ObjectId
+import tweetfilter
+import string
+import re
+from sets import Set
+ 
+ps = PorterStemmer()
+table = string.maketrans("","")
+
+def get_stopwords():
+    stops = stopwords.words('english')
+    stops.extend(["'re", "n't", "'s"])    
+    return stops  
+
+#thanks to http://stackoverflow.com/questions/3845423/remove-empty-strings-from-a-list-of-strings
+def strip_regex(word):       
+    #return re.sub(r'[^\w\s]','',word)  #remove word only
+    return re.sub(r'[^a-zA-Z\s]','',word) #remove number as well
+
+def strip_trans(s):
+    return s.translate(None, string.punctuation)
+
+def tokenize_tweets(text):    
+    text = tweetfilter.clean_tweet(text)    
+    return [ps.stem(w) for s in sent_tokenize(text) for w in word_tokenize(s)]
+
+def handle(text, tweet_id):    
+    tokens = list(Set([strip_trans(token) for token in tokenize_tweets(text.encode("utf-8")) if token not in get_stopwords()]))
+    tokens = filter(None, tokens)     
+    mongo.db.sample_tweets.update({'_id': ObjectId(tweet_id)}, 
+                                  { '$set': {'terms':  tokens } })
+    print "update: ", tokens
+    
+if __name__ == '__main__':
+    for tweet in mongo.db.sample_tweets.find():
+        handle(tweet["text"], tweet["_id"])
+
+    
