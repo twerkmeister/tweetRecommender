1b6de186aa977563a83b546ea9a3a83ef46a152a
christian.godde <christian.godde@student.hpi.uni-potsdam.de>
2014-05-05 12:23:48 +0200

    refactoring

--- tweetRecommender/bow.py
+++ tweetRecommender/bow.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 from gensim import corpora, models, similarities
 import string
 import logging
+import os.path
 
 class MongoCorpus(object):
 	def __init__(self, dictionary):
@@ -14,12 +15,7 @@ class MongoCorpus(object):
 		for doc in subset():
 			yield dictionary.doc2bow(tokenize(doc))
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-minlength = 3
 ps = PorterStemmer()
-stops = stopwords.words('english')
-stops.extend(["'re", "n't", "'s"])
-stops.extend(string.punctuation)
 
 def subset():
 	return mongo.db.sample_webpages.find()
@@ -27,47 +23,49 @@ def subset():
 def tokenize(doc):
 	return [ps.stem(w) for s in sent_tokenize(doc["content"].lower()) for w in word_tokenize(s)]
 
-def get_lda_model():
-	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-	dictionary = corpora.Dictionary.load("tmp/mongocorpus.dict")
-	mm = corpora.MmCorpus("tmp/corpus.mm")
+def get_model(dictionary, corpus):
+	tfidf = models.TfidfModel(corpus)
+	corpus_tfidf = tfidf[corpus]
 
-	tfidf = models.TfidfModel(mm)
-	corpus_tfidf = tfidf[mm]
-
-	model = models.ldamodel.LdaModel(corpus_tfidf, id2word=dictionary, num_topics=100, iterations=5000)
+	model = models.ldamodel.LdaModel(corpus_tfidf, id2word=dictionary, num_topics=200, iterations=5000)
 	return model
 
+def create_dictionary(path, overwrite=False):
+	if os.path.isfile(path) and not overwrite:
+		return corpora.Dictionary.load(dict_path)
+
+	stops = stopwords.words('english')
+	stops.extend(["'re", "n't", "'s"])
+	stops.extend(string.punctuation)
+
+	minlength = 3
+
+	dictionary = corpora.Dictionary(tokenize(doc) for doc in subset())
+	#remove terms occur only in single document
+	once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
+	#remove terms length less than minimal length 	
+	less_ids = [dictionary.token2id[tokenid] for tokenid in dictionary.token2id if len(tokenid) <= minlength]
+	dictionary.filter_tokens([dictionary.token2id[stopword] for stopword in stops if stopword in dictionary.token2id])
+	dictionary.filter_tokens(once_ids + less_ids)
+	dictionary.compactify()
+	dictionary.save(path)
+	return dictionary
+
+def create_corpus(path, overwrite=False):
+	if os.path.isfile(path) and not overwrite:
+		return corpora.MmCorpus(corpus_path)
+
+	corpus = MongoCorpus(dictionary)
+	corpora.MmCorpus.serialize(path, corpus)
+	return corpus
+
 if __name__ == '__main__':
-	# Building Dictionary
-	dictionary = corpora.Dictionary.load("tmp/mongocorpus.dict")
+	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-	if False:
-		dictionary = corpora.Dictionary(tokenize(doc) for doc in subset())
-		print dictionary
-		#remove terms occur only in single document
-		once_ids = [tokenid for tokenid, docfreq in dictionary.dfs.iteritems() if docfreq == 1]
-		#remove terms length less than minimal length 	
-		less_ids = [dictionary.token2id[tokenid] for tokenid in dictionary.token2id if len(tokenid) <= minlength]
-		dictionary.filter_tokens([dictionary.token2id[stopword] for stopword in stops if stopword in dictionary.token2id])
-		dictionary.filter_tokens(once_ids + less_ids)
-		dictionary.compactify()
-		print dictionary
+	dict_path = "tmp/mongocorpus.dict"
+	corpus_path = "tmp/corpus.mm"
 
-		dictionary.save("tmp/mongocorpus.dict")
+	dictionary = create_dictionary(dict_path)
+	corpus = create_corpus(corpus_path)
 
-		# Working with corpus
-		corpus = MongoCorpus(dictionary)
-
-		corpora.MmCorpus.serialize("tmp/corpus.mm", corpus)
-
-	mm = corpora.MmCorpus("tmp/corpus.mm")
-	print mm
-
-	tfidf = models.TfidfModel(mm)
-	corpus_tfidf = tfidf[mm]
-
-	model = models.ldamodel.LdaModel(corpus_tfidf, id2word=dictionary, num_topics=100, iterations=5000)
-	model.save("tmp/model.lsi")
-	print model
-	print model.show_topics()
+	get_model(dictionary, corpus)
