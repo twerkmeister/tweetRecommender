f244d1e39d8befdda09ba1ea9d15b9c7d360f1ec
krizen <christian.godde@gmail.com>
2014-05-07 12:32:09 +0200

    indentation

--- tweetRecommender/evaluation.py
+++ tweetRecommender/evaluation.py
@@ -2,32 +2,32 @@ from tweetRecommender.mongo import mongo
 from tweetRecommender import query
 
 def gold_standard(uri):
-	tweets = mongo.coll("sample_tweets")
-	tweets.find({"full_urls": uri})
-	return tweets
+    tweets = mongo.coll("sample_tweets")
+    tweets.find({"full_urls": uri})
+    return tweets
 
 def evaluate_webpage(uri):
-	reference = gold_standard(uri)
-	result = query.query(uri)
+    reference = gold_standard(uri)
+    result = query.query(uri)
 
-	found = 0
-	combinedScore = 0
+    found = 0
+    combined_score = 0
 
-	for tweet, score in result:
-		if tweet in reference:
-			found += 1
-			combinedScore += score
+    for tweet, score in result:
+        if tweet in reference:
+            found += 1
+            combinedScore += score
 
-	return (found, combinedScore)
+    return (found, combined_score)
 
 def testset():
-	return mongo.coll("sample_webpages_test").find()
+    return mongo.coll("sample_webpages_test").find()
 
 def main():
-	testset = testset()
-	result = []
-	for doc in testset:
-		result.append(evaluate_webpage(doc["url"]))
+    testset = testset()
+    result = []
+    for doc in testset:
+        result.append(evaluate_webpage(doc["url"]))
 
 if __name__ == '__main__':
-	main()
+    main()
--- tweetRecommender/tweetfilter.py
+++ tweetRecommender/tweetfilter.py
@@ -5,10 +5,10 @@ def check(tweet):
     return check_size(tweet.text) and check_author_credibility(tweet)
 
 def check_size(text):
-	cleaned = clean_tweet(text)
-	return len(cleaned) >= config['tweet_min_length']
+    cleaned = clean_tweet(text)
+    return len(cleaned) >= config['tweet_min_length']
 
-def clean_tweet(text):	     
+def clean_tweet(text):       
     # thanks to http://ravikiranj.net/drupal/201205/code/machine-learning/how-build-twitter-sentiment-analyzer
     text = text.lower() #Convert to lower case    
     text = re.sub('((www\.[\s]+)|(https?://[^\s]+))','',text) #Convert www.* or https?://* to EMPTY STRING    
@@ -23,18 +23,18 @@ def clean_tweet(text):
     return text
 
 def check_author_credibility(user):
-	if not user['verified']:
-		return False
-	if user['followers_count'] < config['followers_count']:
-		return False
+    if not user['verified']:
+        return False
+    if user['followers_count'] < config['followers_count']:
+        return False
 
-	#XXX useful?
-	#if not whitelist(user['user_id']):
-	#	return False
-	#if blacklist(user['user_id']):
-	#	return False
+    #XXX useful?
+    #if not whitelist(user['user_id']):
+    #   return False
+    #if blacklist(user['user_id']):
+    #   return False
 
-	return True
+    return True
     
 if __name__ == "__main__":        
     print clean_tweet("RT @RevolutionSyria: (08-15-13) #Daraa #Syria l Rebels Lay Siege to #Assad Hagana Battalion by border in Daraa http://t.co/RMfQhlU2Jr https://www.test.com")    
