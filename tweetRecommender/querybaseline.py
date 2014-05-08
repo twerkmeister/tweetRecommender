31a8cd9bfc33f77f8d4a8fc4276e66d35eb2f5d3
easten20 <easten_20@hotmail.com>
2014-05-08 14:13:12 +0200

    edit querybaseline find terms in index

--- tweetRecommender/querybaseline.py
+++ tweetRecommender/querybaseline.py
@@ -2,30 +2,27 @@ from __future__ import division
 from tweetRecommender.mongo import mongo
 import tweettokenization
 from sets import Set
-import sys
-from bson.objectid import ObjectId
 from Queue import PriorityQueue 
+import sys
 
 def gather(url):
     webpages_coll = mongo.db.sample_webpages
     tweets_coll = mongo.db.sample_tweets
     query = {"url" : url}
     webpage = webpages_coll.find_one(query)
-    tweets = tweets_coll.find()        
-    return (webpage, tweets)                                        
+    news_terms = Set(tweettokenization.tokenize_tweets(webpage["content"].encode("utf-8")));
+    tweets = tweets_coll.find({ "hashtags": { "$in": list(news_terms) } })
+    return (webpage, tweets, news_terms)                                        
 
-def rank(webpage, tweets, topK=10):
-    news_terms = Set(tweettokenization.tokenize_tweets(webpage["content"].encode("utf-8")));    
-    queue = PriorityQueue(topK)
+def rank(webpage, tweets, news_terms, topK=10):        
+    queue = PriorityQueue(topK)        
     def calculate_score(tweet):
         tweet_terms = Set(tweet["terms"])
         intersection = news_terms.intersection(tweet_terms)        
         union = news_terms.union(tweet_terms)   
-        score = len(intersection) / len(union)
-        print score
-        return score
-    
-    for tweet in tweets:
+        score = len(intersection) / len(union)        
+        return score    
+    for tweet in tweets:        
         score = calculate_score(tweet)
         if not queue.full():
             queue.put((score, tweet)) 
@@ -37,16 +34,16 @@ def rank(webpage, tweets, topK=10):
     return queue.queue
 
 
-def main(uri):
-    webpage,tweets = gather(uri)
-    ranked_tweets = rank(webpage, tweets)
-    print("Ranking:")
-    for score, tweet in ranked_tweets:
+def main(uri):    
+    webpage,tweets, news_terms = gather(uri)
+    ranked_tweets = rank(webpage, tweets, news_terms)
+    print("Ranking:")    
+    for score, tweet in ranked_tweets:        
         print("[%.2f] text: %s" % (score, tweet["text"].encode("utf-8")))
 
 
 if __name__ == "__main__":        
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 2:     
         print("please provide a url")
         sys.exit(1)
     main(sys.argv[1])
