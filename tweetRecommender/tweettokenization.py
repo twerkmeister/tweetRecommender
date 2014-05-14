3bbbd99016fc3a976ec859694931bb17fdc66a40
Robert Lehmann <mail@robertlehmann.de>
2014-05-14 12:26:00 +0200

    Refactor.

--- tweetRecommender/tweettokenization.py
+++ tweetRecommender/tweettokenization.py
@@ -1,37 +1,33 @@
-import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 from tweetRecommender.mongo import mongo
-from bson.objectid import ObjectId
+from tweetRecommender import tweetfilter
 import functools32
-import tweetfilter
-import string
-import re
-from sets import Set
- 
+
 ps = PorterStemmer()
 
+@functools32.lru_cache()
 def get_stopwords():
     stops = stopwords.words('english')
-    stops.extend(["'re", "n't", "'s"])    
-    return stops  
+    stops.extend(["'re", "n't", "'s"])
+    return stops
 
 def get_terms(text):
     return list(set(tokenize_tweets(text)))
 
 @functools32.lru_cache()
-def tokenize_tweets(text):    
-    text = tweetfilter.clean_tweet(text)    
-    return [ps.stem(w) for w in word_tokenize(text) if not w in get_stopwords()]    
+def tokenize_tweets(text):
+    text = tweetfilter.clean_tweet(text)
+    return [ps.stem(w) for w in word_tokenize(text)
+            if not w in get_stopwords()]
 
 def handle(tweet, bulk):
     text = tweet["text"]
     tweet_id = tweet["_id"]
-    tokens = list(Set([token for token in tokenize_tweets(text.encode("utf-8"))]))    
+    tokens = list(set(tokenize_tweets(text.encode("utf-8"))))
     bulk.find({'_id': tweet_id}).update({'$set': {'terms': tokens}})
-    print "update: ", tokens
-    
+
 if __name__ == '__main__':
     bulk = mongo.db.sample_tweets.initialize_unordered_bulk_op()
     for tweet in mongo.db.sample_tweets.find():
