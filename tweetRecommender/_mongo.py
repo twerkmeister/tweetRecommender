fe5deb2e8f0a0a7dd96377958735ca83e8bb65a0
Robert Lehmann <mail@robertlehmann.de>
2014-05-14 12:04:32 +0200

    Store _id field name.

--- tweetRecommender/_mongo.py
+++ tweetRecommender/_mongo.py
@@ -3,13 +3,15 @@ from pymongo.collection import Collection
 from bson.objectid import ObjectId
 
 class MongoConnector:
+    ID = '_id'
+
     def __init__(self, host, database, username, password):
         self.client = client = MongoClient(host)
         self.db = db = client[database]
         db.authenticate(username, password)
 
     def by_id(self, collection, object_id):
-        return self.db[collection].find_one({'_id': ObjectId(object_id)})
+        return self.db[collection].find_one({self.ID: ObjectId(object_id)})
 
     def coll(self, collection_or_name):
         if isinstance(collection_or_name, basestring):
