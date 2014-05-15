7744d6b23ca53c0f47164b96bb4946289fc9b5a6
easten20 <easten_20@hotmail.com>
2014-05-15 15:42:26 +0200

    remove get_lda and get_dict from similarity implementation to ldamodel

--- tweetRecommender/rank/lda_cossim.py
+++ tweetRecommender/rank/lda_cossim.py
@@ -1,24 +1,14 @@
 from __future__ import division
 
 import tweetRecommender.ldamodel as ldamodel
-from tweetRecommender.config import config
 from tweetRecommender.tokenize import tokenize  
 
-from gensim import models, matutils
-import functools32
-
-@functools32.lru_cache() 
-def get_lda():
-    return models.LdaModel.load('c://tmp//news_lda_model.model')
-    
-@functools32.lru_cache() 
-def get_dictionary():    
-    return ldamodel.create_dictionary(config["lda"]["dict_path"])
+from gensim import matutils
 
 #http://stackoverflow.com/questions/22433884/python-gensim-how-to-calculate-document-similarity-using-the-lda-model   
 def score(tweet, webpage):        
-    lda = get_lda()    
-    dictionary = get_dictionary()    
+    lda = ldamodel.get_lda()    
+    dictionary = ldamodel.get_dictionary()    
     tweet_vec = lda[dictionary.doc2bow(tweet['terms'])]    
     news_vec = lda[dictionary.doc2bow(tokenize(webpage["content"]))]    
     score = matutils.cossim(news_vec, tweet_vec)                    
--- tweetRecommender/rank/lda_hellinger.py
+++ tweetRecommender/rank/lda_hellinger.py
@@ -1,26 +1,16 @@
 from __future__ import division
 
 import tweetRecommender.ldamodel as ldamodel
-from tweetRecommender.config import config
 from tweetRecommender.tokenize import tokenize
 
-from gensim import models, matutils   
+from gensim import matutils   
 import numpy as np
-import functools32
-
-@functools32.lru_cache() 
-def get_lda():
-    return models.LdaModel.load('c://tmp//news_lda_model.model')
-    
-@functools32.lru_cache() 
-def get_dictionary():    
-    return ldamodel.create_dictionary(config["lda"]["dict_path"])
 
 #http://stackoverflow.com/questions/22433884/python-gensim-how-to-calculate-document-similarity-using-the-lda-model
 #similarity using hellinger distance better than cosine similarity for lda model    
 def score(tweet, webpage):
-    lda = get_lda()    
-    dictionary = get_dictionary()
+    lda = ldamodel.get_lda()
+    dictionary = ldamodel.get_dictionary()
     tweet_vec = lda[dictionary.doc2bow(tweet['terms'])]
     news_vec = lda[dictionary.doc2bow(tokenize(webpage["content"]))]
     dense1 = matutils.sparse2full(tweet_vec, lda.num_topics)
