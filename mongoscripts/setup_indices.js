ff6692671cde13e40e92f51c6a8444ad064ad3bb
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2014-05-20 13:40:22 +0200

    added indices for date retrieval

--- mongoscripts/setup_indices.js
+++ mongoscripts/setup_indices.js
@@ -1,7 +1,11 @@
 db.tweets.ensureIndex({full_urls: 1})  // multikey
 db.tweets.ensureIndex({hashtags: 1})  // multikey
+
 db.sample_tweets.ensureIndex({terms: 1})  // multikey
 db.sample_tweets.ensureIndex({hashtags: 1})
 db.sample_tweets.ensureIndex({full_urls: 1})
+db.sample_tweets.ensureIndex({created_at: 1})
+db.sample_tweets.ensureIndex({created_at: 1, terms: 1})
+
 db.webpages.ensureIndex({url: "hashed"})
 db.redirects.ensureIndex({from: "hashed"})
