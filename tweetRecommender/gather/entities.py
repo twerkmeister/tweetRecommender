302075933d4344d8db5664d5499653ebb1a4b68c
easten20 <easten_20@hotmail.com>
2014-05-20 15:28:40 +0200

    add comment to entities extraction

--- tweetRecommender/gather/entities.py
+++ tweetRecommender/gather/entities.py
@@ -1,10 +1,10 @@
-from __future__ import division
 from kales import Kales
 from tweetRecommender.tokenize import get_terms
 
+#entities extraction using opencalais
 API_KEY = "yzutnbsbs668m3qepb5khwxm"
 num_of_entities = 10
-def gather(webpage, tweets, webpages):
+def gather(webpage, tweets):
     kales = Kales(API_KEY)
     data = kales.analyze(webpage['content'].encode('utf-8'))
     terms = list()    
