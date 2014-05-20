a29238d4adea1acc1d1b2430c5673b05264e117f
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2014-05-20 12:36:03 +0200

    added indices

--- mongoscripts/setup_indices.js
+++ mongoscripts/setup_indices.js
@@ -1,5 +1,7 @@
 db.tweets.ensureIndex({full_urls: 1})  // multikey
 db.tweets.ensureIndex({hashtags: 1})  // multikey
 db.sample_tweets.ensureIndex({terms: 1})  // multikey
+db.sample_tweets.ensureIndex({hashtags: 1})
+db.sample_tweets.ensureIndex({full_urls: 1})
 db.webpages.ensureIndex({url: "hashed"})
 db.redirects.ensureIndex({from: "hashed"})
