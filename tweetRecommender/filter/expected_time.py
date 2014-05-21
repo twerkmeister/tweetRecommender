b09891b8e5cfea868b109098bb0b5e8a04295f4b
Robert Lehmann <mail@robertlehmann.de>
2014-05-21 11:16:36 +0200

    Add expected publishing time filter.

--- tweetRecommender/filter/expected_time.py
+++ tweetRecommender/filter/expected_time.py
@@ -1,3 +1,14 @@
-def filter(query):
-    #XXX annotate query by expected time +- stdev
-    return query
+from datetime import timedelta
+
+STANDARD_DEVIATION_HOURS = 43
+STANDARD_EXPECTATION = 6
+_SHIFT = timedelta(hours = STANDARD_EXPECTATION)
+_STDDEV = timedelta(hours = STANDARD_DEVIATION_HOURS)
+
+def filter(webpage):
+    created_at = webpage['created_at'] + _SHIFT
+    lower_bound = created_at - _STDDEV
+    upper_bound = created_at + _STDDEV
+    return {
+            'created_at': {'$gt': lower_bound, '$lt': upper_bound},
+           }
