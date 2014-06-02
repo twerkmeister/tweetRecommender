eef9a79a13d0f6a8795846d5a004a2b582c7f840
easten20 <easten_20@hotmail.com>
2014-06-02 14:59:27 +0200

    show result from multiple ranking

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -20,25 +20,29 @@ def query():
 		gather = request.form.get('gather')
 		ranking = request.form.get('ranking')
 		action = request.form.get('action')
-
-		try:
-			if action == "search":
-				url = request.form.get('url')
-
-			elif action == "random":
-				random_max = mongo.db["sample_webpages"].count() - 1
-				random_webpage = mongo.db["sample_webpages"].find().skip(
-					randint(0,random_max)).limit(1)[0]
-				url = random_webpage.get('url')
-                        else:
-				raise ValueError("invalid action")
-
-			tweets = recommend(url, gather, [ranking], ['expected_time'],
-				['user.screen_name', 'created_at', 'text'],
-				'sample_tweets', 'sample_webpages', limit)
-			return render_template('result.html', url=url, tweets=tweets)
-
-		except Exception, e:
-			import traceback; traceback.print_exc()
-			return render_template('result.html', url=e)
-		
+        gather1 = request.form.get('gather1')
+        ranking1 = request.form.get('ranking1')                
+        try:        
+            if action == "search":
+                url = request.form.get('url')
+            elif action == "random":
+                random_max = mongo.db["sample_webpages"].count() - 1
+                random_webpage = mongo.db["sample_webpages"].find().skip(randint(0, random_max)).limit(1)[0]
+                url = random_webpage.get('url')  
+            else:
+			     raise ValueError("invalid action")
+            
+            tweets = recommend(url, gather, [ranking], ['expected_time'], 
+                               ['user.screen_name', 'created_at', 'text'], 
+                               'sample_tweets', 'sample_webpages', limit)
+		  
+            if ranking1 != "" and gather1 != "":
+                tweets1 = recommend(url, gather1, [ranking1], ['expected_time'],
+                                    ['user.screen_name', 'created_at', 'text'], 'sample_tweets',
+                                    'sample_webpages', limit)
+                return render_template('result.html', url=url, tweets=tweets, tweets1=tweets1)
+            else:
+                return render_template('result.html', url=url, tweets=tweets)
+        except Exception, e:
+		    import traceback; traceback.print_exc()
+		    return render_template('result.html', url=e)		
