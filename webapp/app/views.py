da094caa24d3594a1f70b341a5d8145fd9a2c35d
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2014-06-04 21:13:57 +0200

    rudimentary implementation

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -2,26 +2,59 @@ from tweetRecommender.query import run as recommend
 from tweetRecommender.mongo import mongo
 
 from app import app
-from flask import render_template, request, url_for, redirect, jsonify
+from flask import render_template, request, url_for, redirect, jsonify, send_file
 
 from random import randint
 
 @app.route("/", methods=['GET'])
-def home():
-    return render_template('index.html')
+def index():
+    return send_file('templates/index.html')
 
-@app.route("/query", methods=['GET', 'POST'])
+# @app.route("/query", methods=['GET', 'POST'])
+# def query():
+# 	if request.method == 'GET':
+# 		return render_template('query.html')
+
+# 	if request.method == 'POST':	
+# 		limit = int(request.form.get('limit'))
+# 		gather = request.form.get('gather')
+# 		ranking = request.form.get('ranking')
+# 		action = request.form.get('action')
+#         gather1 = request.form.get('gather1')
+#         ranking1 = request.form.get('ranking1')                
+#         try:        
+#             if action == "search":
+#                 url = request.form.get('url')
+#             elif action == "random":
+#                 random_max = mongo.db["sample_webpages"].count() - 1
+#                 random_webpage = mongo.db["sample_webpages"].find().skip(randint(0, random_max)).limit(1)[0]
+#                 url = random_webpage.get('url')  
+#             else:
+# 			     raise ValueError("invalid action")
+            
+#             tweets = recommend(url, gather, [ranking], ['expected_time'], 
+#                                ['user.screen_name', 'created_at', 'text'], 
+#                                'sample_tweets', 'sample_webpages', limit)
+		  
+#             if ranking1 != "" and gather1 != "":
+#                 tweets1 = recommend(url, gather1, [ranking1], ['expected_time'],
+#                                     ['user.screen_name', 'created_at', 'text'], 'sample_tweets',
+#                                     'sample_webpages', limit)
+#                 return render_template('result.html', url=url, tweets=tweets, tweets1=tweets1)
+#             else:
+#                 return render_template('result.html', url=url, tweets=tweets)
+#         except Exception, e:
+# 		    import traceback; traceback.print_exc()
+# 		    return render_template('result.html', url=e)		
+
+@app.route("/query", methods=['POST'])
 def query():
-	if request.method == 'GET':
-		return render_template('query.html')
-
-	if request.method == 'POST':	
-		limit = int(request.form.get('limit'))
-		gather = request.form.get('gather')
-		ranking = request.form.get('ranking')
-		action = request.form.get('action')
-        gather1 = request.form.get('gather1')
-        ranking1 = request.form.get('ranking1')                
+    if request.method == 'POST':    
+        limit = 10
+        gather = "terms"
+        ranking = "text_overlap"
+        action = "random"
+        result = {"tweets": []}    
         try:        
             if action == "search":
                 url = request.form.get('url')
@@ -30,22 +63,19 @@ def query():
                 random_webpage = mongo.db["sample_webpages"].find().skip(randint(0, random_max)).limit(1)[0]
                 url = random_webpage.get('url')  
             else:
-			     raise ValueError("invalid action")
+                 raise ValueError("invalid action")
             
-            tweets = recommend(url, gather, [ranking], ['expected_time'], 
+            result["tweets"] = recommend(url, gather, [ranking], ['expected_time'], 
                                ['user.screen_name', 'created_at', 'text'], 
                                'sample_tweets', 'sample_webpages', limit)
-		  
-            if ranking1 != "" and gather1 != "":
-                tweets1 = recommend(url, gather1, [ranking1], ['expected_time'],
-                                    ['user.screen_name', 'created_at', 'text'], 'sample_tweets',
-                                    'sample_webpages', limit)
-                return render_template('result.html', url=url, tweets=tweets, tweets1=tweets1)
-            else:
-                return render_template('result.html', url=url, tweets=tweets)
+            
+            for score, tweet in result["tweets"]:
+                tweet.pop("_id")
+
+            return jsonify(result)
         except Exception, e:
-		    import traceback; traceback.print_exc()
-		    return render_template('result.html', url=e)		
+            import traceback; traceback.print_exc()
+            return jsonify(result)
 
 @app.route("/ranking")
 def ranking():
