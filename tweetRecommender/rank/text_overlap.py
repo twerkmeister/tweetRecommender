c8dbc5e71be604a0736e8397e6c81ae13f851479
easten20 <easten_20@hotmail.com>
2014-06-04 12:01:50 +0200

    combine the length of tweets and news terms as normalization factor

--- tweetRecommender/rank/text_overlap.py
+++ tweetRecommender/rank/text_overlap.py
@@ -6,7 +6,7 @@ def score(tweet, webpage):
     tweet_terms = set(tweet['terms'])
     news_terms = set(tokenize(webpage['content'].encode('utf-8')))
     intersection = news_terms.intersection(tweet_terms)
-    score = len(intersection) / len(tweet_terms)
+    score = len(intersection) / (len(tweet_terms) + len(news_terms))
     return score
 
 score.fields = ['terms']
