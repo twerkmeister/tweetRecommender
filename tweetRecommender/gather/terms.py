c713f99b263107907da318a7c4e8bc0b1a618118
Robert Lehmann <mail@robertlehmann.de>
2014-06-18 11:52:12 +0200

    Required terms matching to find at least n terms.

--- tweetRecommender/gather/terms.py
+++ tweetRecommender/gather/terms.py
@@ -2,6 +2,35 @@ from __future__ import division
 
 from tweetRecommender.tokenize import get_terms
 
+import bson.code
+
+TRESHOLD = 4
+CLAUSE = '''
+function() {
+  var c = 0
+    , this_terms = this.terms
+    , this_total = this.terms.length
+    , terms_total = terms.length
+    ;
+  for (var i = 0; i < this_total; i++) {
+    for (var j = 0; j < terms_total; j++) {
+      if (this_terms[i] == terms[j]) {
+        c++;
+        break;
+      }
+    }
+  }
+  return c > treshold;
+}
+'''
+
+
 def gather(webpage):
     terms = get_terms(webpage['content'].encode('utf-8'))
-    return {'terms': {'$in': terms}}
+    return {
+        'terms': {'$in': terms},
+        '$where': bson.code.Code(CLAUSE,
+            treshold = TRESHOLD,
+            terms = terms,
+        ),
+    }
