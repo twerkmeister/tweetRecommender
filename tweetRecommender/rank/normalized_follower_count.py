d7c2fedce13967da060c0a93378bfc018edebe42
Robert Lehmann <mail@robertlehmann.de>
2014-07-07 12:09:51 +0200

    Guard against a user's first tweet.

--- tweetRecommender/rank/normalized_follower_count.py
+++ tweetRecommender/rank/normalized_follower_count.py
@@ -3,6 +3,9 @@ from __future__ import division
 DISPLAY_NAME = "User popularity"
 
 def score(tweet, webpage):
-    return tweet["user"]["followers_count"] / tweet["user"]["statuses_count"]
+    statuses = tweet["user"]["statuses_count"]
+    if not statuses:
+        statuses = 1
+    return tweet["user"]["followers_count"] / statuses
 
 score.fields = ['user.followers_count', 'user.statuses_count']
