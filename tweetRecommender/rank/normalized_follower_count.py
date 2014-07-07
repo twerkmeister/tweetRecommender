aa43221eba13007953f4ad7995583052d358ed58
Robert Lehmann <mail@robertlehmann.de>
2014-07-07 12:17:12 +0200

    Revert d7c2fed;  instead, add current tweet to statuses_count.

--- tweetRecommender/rank/normalized_follower_count.py
+++ tweetRecommender/rank/normalized_follower_count.py
@@ -3,9 +3,7 @@ from __future__ import division
 DISPLAY_NAME = "User popularity"
 
 def score(tweet, webpage):
-    statuses = tweet["user"]["statuses_count"]
-    if not statuses:
-        statuses = 1
-    return tweet["user"]["followers_count"] / statuses
+    return tweet["user"]["followers_count"] / (
+           tweet["user"]["statuses_count"] + 1)
 
 score.fields = ['user.followers_count', 'user.statuses_count']
