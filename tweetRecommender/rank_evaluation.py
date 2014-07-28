37194bda8c27cc2ab5421f80dd153de6be2dae85
easten20 <easten_20@hotmail.com>
2014-07-28 18:08:35 +0200

    update rank evaluation

--- tweetRecommender/rank_evaluation.py
+++ tweetRecommender/rank_evaluation.py
@@ -2,6 +2,7 @@ from __future__ import division
 from tweetRecommender.mongo import mongo
 from tweetRecommender.query import run
 from bson import ObjectId
+import functools32
 
 TWEETS_COLLECTION = 'tweets'
 WEBPAGES_COLLECTION = 'webpages'
@@ -13,6 +14,7 @@ EVALUATION_FILTERS = []
 EVALUATION_RANKERS = ['lda_cossim', 'language_model', 'text_overlap, normalized_follower_count']
 CACHED_RESULTS_COLLECTION = 'evaluation_cache_advanced'
 
+@functools32.lru_cache()
 def get_evaluated_collection(url):
     value = []
     collection = mongo.coll("evaluation").aggregate([{"$match" : { "webpage" : url}},
@@ -26,25 +28,36 @@ def get_evaluated_collection(url):
             value.append(ObjectId(tweet["_id"]))
     return set(value)    
 
-def evaluate_rank(query_url):    
-    collections = get_evaluated_collection(query_url)
-    ranks = dict()    
-    for ranker in EVALUATION_RANKERS:
-        relevants = 0
-        position = 0 
-        precisions = []       
-        rankers = ranker.split(',')
-        ranker_result = run(url=query_url, gatherer=EVALUATION_GATHERER, rankers=rankers,
-                            filters=EVALUATION_FILTERS, fields=['user.screen_name', 'created_at', 'text'],
-                            tweets_ref=TWEETS_SUBSAMPLE, webpages_ref=WEBPAGES_SUBSAMPLE, limit=10)                    
-        for _, tweet in ranker_result:
-            position += 1                    
-            if (ObjectId(tweet["_id"]) in collections):                                
-                relevants += 1
-                precisions.append(relevants/position)                                                
-        print ("evaluation algorithm: %s" % ranker)
-        print ("number of relevant tweets: %s" % relevants)
-        print ("Mean Average Precision: %f" % (sum(precisions)/relevants))                    
+def calculate_MAP(query_url, ranker):    
+    collections = get_evaluated_collection(query_url)        
+    relevants = 0
+    position = 0 
+    precisions = []       
+    rankers = ranker.split(',')
+    ranker_result = run(url=query_url, gatherer=EVALUATION_GATHERER, rankers=rankers,
+                        filters=EVALUATION_FILTERS, fields=['user.screen_name', 'created_at', 'text'],
+                        tweets_ref=TWEETS_SUBSAMPLE, webpages_ref=WEBPAGES_SUBSAMPLE, limit=10)                    
+    for _, tweet in ranker_result:
+        position += 1                    
+        if (ObjectId(tweet["_id"]) in collections):                                
+            relevants += 1
+            precisions.append(relevants/position)            
+    print ("evaluation algorithm: %s" % ranker)
+    print ("number of relevant tweets: %s" % relevants)
+    print ("Mean Average Precision: %f" % (sum(precisions)/relevants))
+    return (sum(precisions)/relevants)         
 
-if __name__ == '__main__':    
-    evaluate_rank("http://www.washingtonpost.com/blogs/worldviews/wp/2013/08/06/some-girls-have-been-married-60-times-by-the-time-they-turn-18/")
+def evaluate_collections():
+    ranks = dict()
+    count = 0
+    for webpage in mongo.coll(CACHED_RESULTS_COLLECTION).find({},{"query_url":1}):
+        count += 1        
+        for ranker in EVALUATION_RANKERS:
+            values = 0                        
+            if ranker in ranks:                
+                values = ranks.get(ranker)            
+            ranks[ranker] = values + calculate_MAP(webpage["query_url"], ranker)                                            
+    for ranker in EVALUATION_RANKERS:
+        print "Average MAP %s : %f" % (ranker, (ranks[ranker]/count))              
+if __name__ == '__main__':
+    evaluate_collections()    
