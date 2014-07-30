aa10c02a57943f07a3f079bd53d290b9761dfcd0
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2014-07-30 21:53:27 +0200

    fixed labels

--- dataAnalysis/classDistribution.r
+++ dataAnalysis/classDistribution.r
@@ -6,9 +6,9 @@ normalized = data.frame(ee[[2]], (ee[[5]]-mean(ee[[5]], na.rm=T)) / sd(ee[[5]], 
 
 
 means = c(aggregate(normalized[1], by=normalized[2], FUN=mean), aggregate(normalized[1], by=normalized[3], FUN=mean), aggregate(normalized[1], by=normalized[4], FUN=mean))
-plot(normalized[[2]], normalized[[1]], main=names[1])
+plot(normalized[[2]], normalized[[1]], main=names[1], ylab="label", xlab=paste("normalized", names[1],"score"))
 lines(means[[1]], means[[2]])
-plot(normalized[[3]], normalized[[1]], main=names[2])
+plot(normalized[[3]], normalized[[1]], main=names[2], ylab="label", xlab=paste("normalized", names[2],"score"))
 lines(means[[3]], means[[4]])
-plot(normalized[[4]], normalized[[1]], main=names[3])
+plot(normalized[[4]], normalized[[1]], main=names[3], ylab="label", xlab=paste("normalized", names[3],"score"))
 lines(means[[5]], means[[6]])
--- dataAnalysis/scoreDistribution.r
+++ dataAnalysis/scoreDistribution.r
@@ -6,5 +6,5 @@ names = c("lda", "language model", "text overlap")
 unique_pairs = ee[!duplicated(data.frame(ee$webpage, ee$tweet)),]
 
 for(i in 5:7){
-  hist((unique_pairs[[i]]-mean(unique_pairs[[i]], na.rm=T)) / sd(unique_pairs[[i]], na.rm=T), breaks=20, main=names[i-4], sub="test")
+  hist((unique_pairs[[i]]-mean(unique_pairs[[i]], na.rm=T)) / sd(unique_pairs[[i]], na.rm=T), breaks=20, main=names[i-4], ylab="count", xlab=paste("normalized",names[i-4], "score"))
 }
