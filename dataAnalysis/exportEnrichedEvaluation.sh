e7c936f0ece530dd0cc4c32e5f8a9faee3574e73
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2014-07-30 21:28:56 +0200

    corrected host

--- dataAnalysis/exportEnrichedEvaluation.sh
+++ dataAnalysis/exportEnrichedEvaluation.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mongoexport --host localhost -u mpss14n -p twitter -d twitter_subset -c evaluation_enriched --csv --fields webpage,rating,tweet,uid,scores.lda_cossim,scores.language_model,scores.text_overlap > `dirname $0`/../dump/twitter_subset/evaluationEnriched.csv
+mongoexport --host 72.16.22.219 -u mpss14n -p twitter -d twitter_subset -c evaluation_enriched --csv --fields webpage,rating,tweet,uid,scores.lda_cossim,scores.language_model,scores.text_overlap > `dirname $0`/../dump/twitter_subset/evaluationEnriched.csv
