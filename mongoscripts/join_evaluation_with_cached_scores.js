59865494d345c54221b562a3012dcad5de46cb08
krizen <christian.godde@gmail.com>
2014-08-20 14:24:10 +0200

    added times object which contains multiple time values

--- mongoscripts/join_evaluation_with_cached_scores.js
+++ mongoscripts/join_evaluation_with_cached_scores.js
@@ -1,6 +1,17 @@
-function get_absolute_time(query_url, tweet_creation_time) {
-  webpage_creation_time = db.sample_webpages.findOne({url: query_url}).created_at
+function get_absolute_time(webpage_creation_time, tweet_creation_time) {
   return Math.abs(webpage_creation_time - tweet_creation_time)
+}
+
+function get_relative_time(webpage_creation_time, tweet_creation_time) {
+  return Math.abs(webpage_creation_time - tweet_creation_time)
+}
+
+function get_time_values(query_url, tweet_creation_time) {
+  webpage_creation_time = db.sample_webpages.findOne({url: query_url}).created_at
+  time_values = {}
+  time_values.absolute_time_difference = get_absolute_time(webpage_creation_time, tweet_creation_time)
+  time_values.relative_time_difference = get_relative_time(webpage_creation_time, tweet_creation_time)
+  return time_values
 }
 
 db.evaluation_cache_advanced.find().forEach(function(cachedResult){
@@ -10,7 +21,7 @@ db.evaluation_cache_advanced.find().forEach(function(cachedResult){
       evaluation.scores.lda_cossim = tweet.scores[0].lda_cossim
       evaluation.scores.language_model = tweet.scores[1].language_model
       evaluation.tweet_length = tweet.tweet.terms.length // number of terms after stopword removal and stemming
-      evaluation.absolute_time_difference = get_absolute_time(cachedResult.query_url, tweet.tweet.created_at)
+      evaluation.times = get_time_values(cachedResult.query_url, tweet.tweet.created_at)
       db.evaluation_enriched.insert(evaluation)
     });
   });
