a5afe61bc0fcbadde3c408e8418cb46fa7634739
krizen <christian.godde@gmail.com>
2014-08-20 14:31:04 +0200

    more time values

--- mongoscripts/join_evaluation_with_cached_scores.js
+++ mongoscripts/join_evaluation_with_cached_scores.js
@@ -3,7 +3,21 @@ function get_absolute_time(webpage_creation_time, tweet_creation_time) {
 }
 
 function get_relative_time(webpage_creation_time, tweet_creation_time) {
-  return Math.abs(webpage_creation_time - tweet_creation_time)
+  return tweet_creation_time - webpage_creation_time
+}
+
+function get_binary_time_decision(webpage_creation_time, tweet_creation_time) {
+  // webpage published... (-1: before, 1: after)
+  if (tweet_creation_time - webpage_creation_time) >= 0
+    return 1
+  return -1
+}
+
+function get_capped_time(webpage_creation_time, tweet_creation_time) {
+  // return 0 for all tweets published before webpage and real value for other
+  if (tweet_creation_time - webpage_creation_time) > 0
+    return (tweet_creation_time - webpage_creation_time)
+  return 0
 }
 
 function get_time_values(query_url, tweet_creation_time) {
@@ -11,6 +25,8 @@ function get_time_values(query_url, tweet_creation_time) {
   time_values = {}
   time_values.absolute_time_difference = get_absolute_time(webpage_creation_time, tweet_creation_time)
   time_values.relative_time_difference = get_relative_time(webpage_creation_time, tweet_creation_time)
+  time_values.binary_decision = get_binary_time_decision(webpage_creation_time, tweet_creation_time) 
+  time_values.capped_time_after = get_capped_time(webpage_creation_time, tweet_creation_time)
   return time_values
 }
 
