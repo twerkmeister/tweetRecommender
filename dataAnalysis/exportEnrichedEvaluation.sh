be78cc53dd17f45c297d6399be07c570ac54d8b4
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2014-08-25 13:18:40 +0200

    exporting the new evaluation

--- dataAnalysis/exportEnrichedEvaluation.sh
+++ dataAnalysis/exportEnrichedEvaluation.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mongoexport --host localhost -u mpss14n -p twitter -d twitter_subset -c evaluation_enriched --csv --fields webpage,rating,tweet,uid,scores.lda_cossim,scores.language_model,scores.text_overlap,tweet_length > `dirname $0`/../dump/twitter_subset/evaluationEnriched.csv
+mongoexport --host 172.16.22.219 -u mpss14n -p twitter -d twitter_subset -c evaluation_enriched --csv --fields webpage,rating,tweet,uid,scores.lda_cossim,scores.language_model,tweet_length,chars,isverified,followers_count,statuses_count,listed_count,friends_count,times.absolute_time_difference,times.relative_time_difference,times.binary_decision,times.capped_time_after,contains_url,url_count,hashtag_count > `dirname $0`/../dump/twitter_subset/evaluationEnriched2.csv
